ba37667abd8a0e2db75a5ad5b49ffe0747fd7a5e
shiko <shayshi@camous.technion.ac.il>
2021-10-21 21:55:07 +0300

    fixed the worst bug in the history of bugs

--- graphgym/contrib/loader/HIVCsvHandler.py
+++ graphgym/contrib/loader/HIVCsvHandler.py
@@ -23,16 +23,17 @@ class ProcessSlideFile(object):
     _LABEL_COL_NAME = 'HIV'
     _X_POS_COL_NAME = 'X_position'
     _Y_POS_COL_NAME = 'Y_position'
+    _POS_NORMALIZATION_FACTOR = 100
     CELL_ID_COL_INDEX = 1
     NEIGHBOR_COL_START_INDEX = 128
 
     def __init__(self, file: str):
         self.file_path = self.FOLDER_DIR.joinpath(file)
-        self.file_name = self.file_path.name
+        self.file_name = self.file_path.stem
         self.file_pointer = pd.read_csv(self.file_path)
         self._dist_mat = None
         self._vertex_ten = None
-        self._pos_arr = None
+        self._pos_ten = None
         self._label = None
 
     @property
@@ -48,10 +49,10 @@ class ProcessSlideFile(object):
         return self._dist_mat
 
     @property
-    def pos_arr(self) -> np.ndarray:
-        if self._pos_arr is None:
-            self._set_pos_arr()
-        return self._pos_arr
+    def pos_ten(self) -> Tensor:
+        if self._pos_ten is None:
+            self._set_pos_ten()
+        return self._pos_ten
 
     @property
     def label(self) -> Tensor:
@@ -59,9 +60,9 @@ class ProcessSlideFile(object):
             self._set_label()
         return self._label
 
-    def _set_vertex_ten(self, dtype=torch.double):
+    def _set_vertex_ten(self):
         rel_cols = self.file_pointer.columns[self._MARKERS_COL_START_INDEX:self._MARKERS_COL_STOP_INDEX + 1]
-        self._vertex_ten = torch.tensor(self.file_pointer.loc[:, rel_cols].values, dtype=dtype)
+        self._vertex_ten = torch.tensor(self.file_pointer.loc[:, rel_cols].values)
 
     def _set_label(self):
         file_key = re.search(self._LABEL_NAME_REGEX, self.file_path.name)[0]
@@ -69,17 +70,19 @@ class ProcessSlideFile(object):
         self._label = torch.tensor(status.values)
 
     def _set_dist_mat(self):
-        self._dist_mat = np.exp(-(distance_matrix(self.pos_arr, self.pos_arr) ** 2))
+        self._dist_mat = np.exp(-distance_matrix(self.pos_ten.numpy(), self.pos_ten.numpy()) ** 2)
 
-    def _set_pos_arr(self):
-        self._pos_arr = self.file_pointer.loc[:, [self._X_POS_COL_NAME, self._Y_POS_COL_NAME]].values
+    def _set_pos_ten(self):
+        locations_normalized = self.file_pointer.loc[:, [self._X_POS_COL_NAME, self._Y_POS_COL_NAME]].values \
+                               / ProcessSlideFile._POS_NORMALIZATION_FACTOR
+        self._pos_ten = torch.tensor(locations_normalized)
 
 
 if __name__ == '__main__':
     file_name = "ROI002_T1.csv"
     ex = ProcessSlideFile(file_name)
     d = ex.dist_mat
-    p = ex.pos_arr
+    p = ex.pos_ten
     v = ex.vertex_ten
     l = ex.label
     print(d)
--- graphgym/contrib/loader/self_suppervision_datasets.py
+++ graphgym/contrib/loader/self_suppervision_datasets.py
@@ -6,18 +6,62 @@ from GraphGym.graphgym.contrib.loader.HIVEdgeValues import HivProcGraphData, Nei
 from torch_geometric.data import Data
 
 
-class HivSelfSupBaseClass(InMemoryDataset, ABC):
-    """ this data set is constructed using the naive definition of neighbors determined by physical touch between
-    cells """
+# class HivSelfSupBaseClass(InMemoryDataset):
+#     """ this data set is constructed using the naive definition of neighbors determined by physical touch between
+#     cells """
+#
+#     def __init__(self, root=ProcessSlideFile.FOLDER_DIR.parent.as_posix(), transform=None, pre_transform=None,
+#                  pre_filter=None):
+#         super(HivSelfSupBaseClass, self).__init__(root, transform, pre_transform, pre_filter)
+#         self.data, self.slices = torch.load(self.processed_paths[0])
+#
+#     @property
+#     def raw_file_names(self):
+#         return [x.is_file() for x in ProcessSlideFile.FOLDER_DIR.iterdir()]
+#
+#     @property
+#     def processed_file_names(self):
+#         return [type(self).__name__ + ".data"]
+#
+#     def download(self):
+#         return []
+#
+#     def process(self):
+#         dataset = list()
+#         for file in self.raw_file_names:
+#             cur_graph = HivProcGraphData(file)
+#             edge_values = cur_graph.get_edge_values(self.get_neighborhood_and_args())
+#
+#             cur_data = Data(x=None,
+#                             edge_index=edge_values.edge_index,
+#                             edge_attr=edge_values.edge_attr,
+#                             y=cur_graph.file_processed.vertex_ten,
+#                             pos=cur_graph.file_processed.pos_ten,
+#                             dist_mat=cur_graph.file_processed.dist_mat,
+#                             name=cur_graph.file_processed.file_name,
+#                             original_label=cur_graph.file_processed.label)
+#             cur_data.num_nodes = cur_graph.file_processed.vertex_ten.shape[0]
+#             dataset.append(cur_data)
+#
+#         data, slices = self.collate(dataset)
+#         torch.save((data, slices), self.processed_paths[0])
+#
+#     @abstractmethod
+#     def get_neighborhood_and_args(self):
+#         pass
+
+
+class HivSelfSupPhysicalDataSet(InMemoryDataset):
+    """Data set of whole slide using physical contact as edges"""
 
     def __init__(self, root=ProcessSlideFile.FOLDER_DIR.parent.as_posix(), transform=None, pre_transform=None,
                  pre_filter=None):
-        super(HivSelfSupBaseClass, self).__init__(root, transform, pre_transform, pre_filter)
+        super(HivSelfSupPhysicalDataSet, self).__init__(root, transform, pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
     def raw_file_names(self):
-        return [x.is_file() for x in ProcessSlideFile.FOLDER_DIR.iterdir()]
+        return [x for x in ProcessSlideFile.FOLDER_DIR.iterdir() if x.is_file()]
 
     @property
     def processed_file_names(self):
@@ -30,7 +74,7 @@ class HivSelfSupBaseClass(InMemoryDataset, ABC):
         dataset = list()
         for file in self.raw_file_names:
             cur_graph = HivProcGraphData(file)
-            edge_values = cur_graph.get_edge_values(self.get_neighborhood_and_args())
+            edge_values = cur_graph.get_edge_values(NeighborhoodType.PHYSICAL_NEIGHBORHOOD)
 
             cur_data = Data(x=None,
                             edge_index=edge_values.edge_index,
@@ -46,25 +90,44 @@ class HivSelfSupBaseClass(InMemoryDataset, ABC):
         data, slices = self.collate(dataset)
         torch.save((data, slices), self.processed_paths[0])
 
-    @abstractmethod
-    def get_neighborhood_and_args(self):
-        pass
 
-
-class HivSelfSupPhysicalDataSet(HivSelfSupBaseClass):
-    """Data set of whole slide using physical contact as edges"""
-
-    def get_neighborhood_and_args(self):
-        return NeighborhoodType.PHYSICAL_NEIGHBORHOOD
-
-
-class HivSelfSupRadiusDataSet(HivSelfSupBaseClass):
+class HivSelfSupRadiusDataSet(InMemoryDataset):
     """Data set of whole slide using distance as edged with a cutoff of radius"""
 
     def __init__(self, root=ProcessSlideFile.FOLDER_DIR.parent.as_posix(), transform=None, pre_transform=None,
                  pre_filter=None, radius=25.0):
-        super(HivSelfSupBaseClass, self).__init__(root, transform, pre_transform, pre_filter)
         self.radius = radius
+        super(HivSelfSupRadiusDataSet, self).__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
 
-    def get_neighborhood_and_args(self):
-        return NeighborhoodType.RADIUS_NEIGHBORHOOD, self.radius
+    @property
+    def raw_file_names(self):
+        return [x for x in ProcessSlideFile.FOLDER_DIR.iterdir() if x.is_file()]
+
+    @property
+    def processed_file_names(self):
+        return [type(self).__name__ + ".data"]
+
+    def download(self):
+        return []
+
+    def process(self):
+        dataset = list()
+        for file in self.raw_file_names:
+            cur_graph = HivProcGraphData(file)
+            edge_values = cur_graph.get_edge_values(NeighborhoodType.RADIUS_NEIGHBORHOOD, radius=self.radius)
+
+            cur_data = Data(x=None,
+                            edge_index=edge_values.edge_index,
+                            edge_attr=edge_values.edge_attr,
+                            y=cur_graph.file_processed.vertex_ten,
+                            pos=cur_graph.file_processed.pos_ten,
+                            dist_mat=cur_graph.file_processed.dist_mat,
+                            name=cur_graph.file_processed.file_name,
+                            original_label=cur_graph.file_processed.label)
+            cur_data.num_nodes = cur_graph.file_processed.vertex_ten.shape[0]
+            dataset.append(cur_data)
+
+        data, slices = self.collate(dataset)
+        torch.save((data, slices), self.processed_paths[0])
+
