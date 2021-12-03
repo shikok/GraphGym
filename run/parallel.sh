c3b51903a0684345daf352424e1660ff0ecfa9e5
shay <shaysh95@gmail.com>
2021-12-03 17:35:11 +0200

    added all the config files

--- run/parallel.sh
+++ run/parallel.sh
@@ -10,7 +10,7 @@ MAIN=${5:-main}
   for CONFIG in "$CONFIG_DIR"/*.yaml; do
     if [ "$CONFIG" != "$CONFIG_DIR/*.yaml" ]; then
       ((CUR_JOBS >= MAX_JOBS)) && wait -n
-      python $MAIN.py --cfg $CONFIG --repeat $REPEAT --mark_done &
+      python -m GraphGym.run.$MAIN --cfg $CONFIG --repeat $REPEAT --mark_done &
       echo $CONFIG
       sleep $SLEEP
       ((++CUR_JOBS))
